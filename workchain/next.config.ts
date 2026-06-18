b1705a8c838c7aaa22308701b03a8e0a5a4570fd
Anvay <anvaybarhate@gmail.com>
2026-06-19 00:28:40 +0530

    fix: next-pwa turbopack build error

--- workchain/next.config.ts
+++ workchain/next.config.ts
@@ -8,6 +8,8 @@ const withPWA = withPWAInit({
   skipWaiting: true,
 });
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {},
+};
 
 export default withPWA(nextConfig);
